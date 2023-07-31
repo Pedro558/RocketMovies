5f46c24c10770905e431304b2bb9436053d82e13
pedro558 <pedroafonso109@gmail.com>
2023-07-31 20:41:28 -0300

    :sparkles: feat: add ensureAuthenticated middleware

--- API/src/routes/notes.routes.js
+++ API/src/routes/notes.routes.js
@@ -1,8 +1,11 @@
 const { Router } = require('express')
 const notesRoutes = Router()
 
+const ensureAuthenticated = require('../middlewares/ensureAutheticated')
 const NotesController = require('../controllers/NotesController')
+
 const notesController = new NotesController
+notesRoutes.use(ensureAuthenticated)
 
 notesRoutes.post('/:user_id', notesController.create)
 notesRoutes.get('/:id', notesController.show)
